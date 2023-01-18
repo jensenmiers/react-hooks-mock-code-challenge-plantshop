b85886e6ce80e3c53be2a6272f15767e014edfd5
jensenmiers <jensenmiers@gmail.com>
2023-01-18 11:35:52 -0800

    first deliverable done

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,15 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plants}) {
+
+  const plantItem = plants.map(plant => <PlantCard key={plant.id} {...plant} />)
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {/* render PlantCards components in here */}
+      {plantItem}
+    </ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const [plants, setPlants] = useState([])
+
+
+  const baseUrl = 'http://localhost:6001/plants'
+  useEffect(() => {
+    fetch(baseUrl)
+      .then(res => res.json())
+      .then(setPlants)
+  }, [] )
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plants}/>
     </main>
   );
 }
